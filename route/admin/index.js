bcac4cf5b416c000b674baf8d02d94c3a6c1cb34
13129900985@163.com <13129900985@163.com>
2020-10-22 10:30:30 +0800

    20201022 commit

--- route/admin/index.js
+++ route/admin/index.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const common = require('../../libs/common');
-module.exports = function() {
-	var router = express.Router();
-	//检查登录状态
-	router.use((req,res,next)=>{
-		if(!req.session['admin_id'] && req.url != '/login'){
-			res.redirect('/admin/login');
-		}else{
-			next();
-		}
-	});
-	//处理登录成功后的页面跳转
-	router.get('/',(req,res)=>{
-		res.render('admin/index.ejs',{});
-	});
-	router.use('/login',require('./login')());
-	router.use('/banners',require('./banners')());
-
-
-	return router;
-};
-=======
 const express=require('express');
 const common=require('../../libs/common');
 
@@ -46,4 +22,3 @@ module.exports=function (){
     router.use('/custom',require('./custom')());
     return router;
 };
->>>>>>> 08d9032c26a797b79758399d332fb46271c57fa9
